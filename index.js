f00a91b02cd030b0f8578f65000fae804585a409
Will <swilson.carlson@hotmail.com>
2022-06-12 09:39:19 -0400

    commit tree e94c20091a0c9676dc09a1d97c8597450c20e216

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    return appendCat = [...cats, name];
+}
+
+function prependCat(name) {
+    return prependCat = [name, ...cats];
+}
+
+function removeLastCat() {
+    return removeLastCat = cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return removeFirstCat = cats.slice(1, cats.length);
+}
